0dc35ec7de7b6da8882a8ab5bf32a98f2cfcd68c
Augustin Husson <augustin.husson@amadeus.com>
2022-06-28 15:29:43 +0200

    review the impl of async to use generic and have a better error management
    Signed-off-by: Augustin Husson <augustin.husson@amadeus.com>

--- async/async.go
+++ async/async.go
@@ -23,27 +23,25 @@
 //	}
 //
 //  func synchronousTask() {
-//  	next := async.Async(func() interface{} {
-//			return doneAsync()
-//  	})
+//  	n := async.Async(doneAsync)
 //		// do some other stuff
 //  	// then wait for the end of the asynchronous task and get back the result
 //  	result := next.Await()
 //  	// do something with the result
 //  }
 //
-// It is useful to use this implementation when you want to paralyze quickly some short function like paralyzing multiple HTTP request.
-// You definitely won't use this implementation if you want to create a cron or a long task. Instead you should implement the interface SimpleTask or Task for doing that.
+// It is useful to use this implementation when you want to paralyze quickly some short function like paralyzing multiple HTTP requests.
+// You definitely won't use this implementation if you want to create a cron or a long task. Instead, you should implement the interface SimpleTask or Task for doing that.
 //
 // As a Task is designed to run in a long term, you will have to take care about a context and a cancel function passed in the parameter of the main method Execute.
 // Note that the context and the cancel function are shared by every task. It's important to understand that because it means if a task is calling the cancel function,
 // then it will close the context for all Task.
 // It means also that all task should listen the context in order to react when the context is closed. Once the context is closed, the task should then perform some action to be stopped properly.
-// Of course it will depend what you are implementing. Not all task need to do something when the context is closed.
-// Also calling the cancel function in the task must done only if your task is critical for your application and so if whatever reason the task doesn't work anymore, then the whole application must be stopped.
+// Of course, it will depend on what you are implementing. Not all task need to do something when the context is closed.
+// Also calling the cancel function in the task must have done only if your task is critical for your application and so if whatever reason the task doesn't work anymore, then the whole application must be stopped.
 // If you are not in this situation, you shouldn't use the cancel function
 //
-// As you may notice, a Task is an extension of the SimpleTask. In addition of the method Execute (coming from the SimpleTask), a Task will need to implement the method Initialize and Finalize.
+// As you may notice, a Task is an extension of the SimpleTask. In addition, of the method Execute (coming from the SimpleTask), a Task will need to implement the method Initialize and Finalize.
 // It can be useful when you want to separate the different phases of the lifecycle of a Task.
 // The Initialize method is called before running the Task (i.e. before calling the method Execute)
 // The Finalize method is called once the context is closed
@@ -56,7 +54,7 @@
 //		SimpleTask
 //	}
 //	func (t *myPeriodicTask) Execute(ctx context.Context, cancelFunc context.CancelFunc) error {
-//		// since my task is quite simple and it will be called periodically, I don't need to listen the context. It is handle by the caller. (For more detail, see taskhelper.Start
+//		// since my task is quite simple, and it will be called periodically, I don't need to listen the context. It is handled by the caller. (For more detail, see taskhelper.Start
 //		logrus.Info("I'm alive!")
 //		return nil
 //	}
@@ -83,39 +81,45 @@ package async
 
 import "context"
 
-type Future interface {
-	Await() interface{}
-	AwaitWithContext(ctx context.Context) interface{}
+func emptyValue[T any]() T {
+	var result T
+	return result
 }
 
-type next struct {
-	await func(ctx context.Context) interface{}
+type Future[T any] interface {
+	Await() T
+	AwaitWithContext(ctx context.Context) (T, error)
 }
 
-func (n *next) Await() interface{} {
-	return n.await(context.Background())
+type next[T any] struct {
+	await func(ctx context.Context) (T, error)
 }
 
-func (n *next) AwaitWithContext(ctx context.Context) interface{} {
+func (n *next[T]) Await() T {
+	result, _ := n.await(context.Background())
+	return result
+}
+
+func (n *next[T]) AwaitWithContext(ctx context.Context) (T, error) {
 	return n.await(ctx)
 }
 
 // Async executes the asynchronous function
-func Async(f func() interface{}) Future {
-	var result interface{}
+func Async[T any](f func() T) Future[T] {
+	var result T
 	// c is going to be used to catch only the signal when the channel is closed.
 	c := make(chan struct{})
 	go func() {
 		defer close(c)
 		result = f()
 	}()
-	return &next{
-		await: func(ctx context.Context) interface{} {
+	return &next[T]{
+		await: func(ctx context.Context) (T, error) {
 			select {
 			case <-ctx.Done():
-				return ctx.Err()
+				return emptyValue[T](), ctx.Err()
 			case <-c:
-				return result
+				return result, nil
 			}
 		},
 	}
